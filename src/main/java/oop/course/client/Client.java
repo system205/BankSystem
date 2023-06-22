17ca280540f6cc319e58a809ab12fc88c42f7653
Georgii Sapronov <qexik@hotmail.com>
2023-06-22 23:12:12 +0300

    Add lanterna (curses-like library) to dependencies. Add simple mockups for login and register page.

---
+++ src/main/java/oop/course/client/Client.java
@@ -0,0 +1,55 @@
+package oop.course.client;
+
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Client
+{
+    private Terminal terminal;
+    public Client()
+    {
+        try
+        {
+            terminal = new DefaultTerminalFactory().createTerminal();
+        }
+        catch (IOException exception)
+        {
+            System.out.println(exception.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new Client().run();
+    }
+
+    public void run()
+    {
+        try {
+            View currentView = new LoginView(terminal);
+            while (true)
+            {
+                var next = currentView.show();
+                boolean close = false;
+                switch (next)
+                {
+                    case NoView -> {
+                        terminal.close();
+                        close = true;
+                    }
+                    case LoginView -> currentView = new LoginView(terminal);
+                    case RegisterView ->  currentView = new RegisterView(terminal);
+                }
+                if (close) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+}
---
+++ src/main/java/oop/course/client/LoginView.java
@@ -0,0 +1,49 @@
+package oop.course.client;
+
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+import java.util.List;
+
+public class LoginView implements View {
+    private final Screen screen;
+    private ViewType nextView;
+
+    public LoginView(Terminal terminal) throws IOException {
+        screen = new TerminalScreen(terminal);
+        nextView = ViewType.NoView;
+    }
+
+    @Override
+    public ViewType show() throws IOException {
+        screen.startScreen();
+        WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
+        Window window = new BasicWindow("BankSystem authentication");
+        window.setHints(List.of(Window.Hint.CENTERED));
+        Panel contentPanel = new Panel(new LinearLayout(Direction.VERTICAL));
+        Label welcomeText = new Label("Welcome to the BankSystem client application!\nPlease, register or login into your existing account.");
+        contentPanel.addComponent(welcomeText);
+        contentPanel.addComponent(new Label("Username"));
+        contentPanel.addComponent(new TextBox());
+        contentPanel.addComponent(new Label("Password"));
+        contentPanel.addComponent(new TextBox().setMask('*'));
+
+        contentPanel.addComponent(new Button("Login", () -> MessageDialog.showMessageDialog(textGUI, "Error", "The error has occured.", MessageDialogButton.OK)));
+        contentPanel.addComponent(new Button("Register page", () -> {
+            nextView = ViewType.RegisterView;
+            window.close();
+        }));
+        contentPanel.addComponent(new Button("Exit", () -> {
+            nextView = ViewType.NoView;
+            window.close();
+        }));
+        window.setComponent(contentPanel);
+        textGUI.addWindowAndWait(window);
+        return nextView;
+    }
+}
