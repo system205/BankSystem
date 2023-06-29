e3953dd406db6880e5d74fa4677add0bfd161e1a
qexik <qexik@hotmail.com>
2023-06-29 10:55:37 +0300

    Change the main loop to have more control of the objects (broken)

--- src/main/java/oop/course/client/TransferView.java
+++ src/main/java/oop/course/client/TransferView.java
@@ -4,24 +4,22 @@ import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class TransferView implements View {
     private final Screen screen;
-    private Type nextView;
+    private Consumer<Type> onSceneChange;
 
-    public TransferView(Terminal terminal) throws IOException {
-        screen = new TerminalScreen(terminal);
-        nextView = Type.None;
+    public TransferView(Screen screen) throws IOException {
+        this.screen = screen;
+        onSceneChange = (Type type) -> {};
     }
 
     @Override
-    public Type show() throws IOException {
-        screen.startScreen();
+    public void show() {
         WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
         Window window = new BasicWindow("Money transfer");
         window.setHints(List.of(Window.Hint.CENTERED));
@@ -34,18 +32,22 @@ public class TransferView implements View {
         contentPanel.addComponent(new TextBox());
 
         contentPanel.addComponent(new Button("Transfer money", () -> {
-            nextView = Type.Account;
             MessageDialog.showMessageDialog(textGUI, "Success", "Successfully transferred money.", MessageDialogButton.OK);
             window.close();
+            onSceneChange.accept(Type.Login);
         }));
 
         contentPanel.addComponent(new Button("Cancel", () -> {
-            nextView = Type.Account;
             window.close();
+            onSceneChange.accept(Type.Account);
         }));
 
         window.setComponent(contentPanel);
-        textGUI.addWindowAndWait(window);
-        return nextView;
+        textGUI.addWindow(window);
+    }
+
+    @Override
+    public void registerChangeViewHandler(Consumer<Type> consumer) {
+        onSceneChange = consumer;
     }
 }
