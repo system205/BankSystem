ba5a2f7549a613f94051d80e0e33d3cda3292a7e
system205 <arsenmutalapov205@gmail.com>
2023-06-30 11:50:06 +0300

    add code to test the system

--- src/main/java/oop/course/Main.java
+++ src/main/java/oop/course/Main.java
@@ -4,14 +4,16 @@ import oop.course.auth.*;
 import oop.course.implementations.*;
 import oop.course.routes.*;
 import oop.course.server.*;
+import oop.course.storage.*;
 
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
+import java.sql.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         System.out.println(Files.walk(Paths.get(".")).filter(Files::isRegularFile).filter(p -> p.getFileName().toString().endsWith("java")).map(path -> {
             try {
                 return Files.lines(path).filter(s -> s.trim().length() > 0 && !s.trim().startsWith("/") && !s.trim().startsWith("*")).count();
@@ -19,13 +21,35 @@ public class Main {
                 throw new RuntimeException(e);
             }
         }).reduce(Long::sum).orElse(0L));
-        final int port = 6666;
+
+        Connection connection = new Postgres(
+                new SimpleNetAddress("127.0.0.1", 5432),
+                new SimpleCredentials("postgres", "postgres"),
+                "bank"
+        ).connect();
+        connection.setAutoCommit(false);
+
+        // Processes
         final Authorization authorization = new Authorization(
                 Optional.of(new Fork(
-                        new FakeUrl(),
+                        new SimpleUrl(),
                         new MainRoute(),
-                        new LoginRoute(new DBLoginCheck())
+                        new LoginRoute(
+                                new DBLoginCheck(connection)
+                        ),
+                        new RegisterRoute(),
+                        new TransferRoute(),
+                        new CheckAccountRoute(
+                                new AccountAccess( // either Forbidden or proceed
+                                        new AccountReturn(
+                                                new CheckingAccountDB()
+                                        )
+                                )
+                        ),
+                        new NotFoundRoute()
                 )));
+
+        final int port = 6666;
         try (ServerSocket socket = new ServerSocket(port)) {
             while (true)
                 new Thread(
--- src/main/java/oop/course/server/Client.java
+++ src/main/java/oop/course/server/Client.java
@@ -18,12 +18,56 @@ public class Client {
         new Client("127.0.0.1", 6666).start();
     }
 
-    public void start() {
+    private void start() {
         try (Socket client = new Socket(this.ip, this.port);
-             PrintWriter out = new PrintWriter(client.getOutputStream(), true)) {
-            System.out.println("Input text:");
-            out.println(new Scanner(System.in).nextLine());
-            out.println(new Scanner(System.in).nextLine());
+             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+            String register = """
+                    POST /register HTTP/1.1
+
+                    {
+                      "username": "admin2",
+                      "password": "admin2"
+                    }
+                    EOF""";
+
+            String login = """
+                    POST /login HTTP/1.1
+
+                    {
+                      "username": "admin2",
+                      "password": "admin2"
+                    }
+                    EOF""";
+
+            String check = """
+                    POST /account HTTP/1.1
+
+                    {
+                      "username": "admin",
+                      "password": "admin"
+                    }
+                    EOF""";
+
+            String transfer = """
+                    POST /transfer HTTP/1.1
+
+                    {
+                      "senderAccount": "admin",
+                      "receiverAccount": "admin"
+                    }
+                    EOF""";
+
+            String random = """
+                    POST /random HTTP/1.1
+                    EOF""";
+
+            final String request = register;
+
+            System.out.println("Sent:\n" + request);
+            out.println(request);
+            System.out.println("\nReceived:");
+            in.lines().forEach(System.out::println);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
