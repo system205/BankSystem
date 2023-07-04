1742156d48be0dcc8cec42de957ba03e5d32c879
qexik <qexik@hotmail.com>
2023-07-04 09:19:26 +0300

    Test the connection to the server

--- src/main/java/oop/course/client/RegisterView.java
+++ src/main/java/oop/course/client/RegisterView.java
@@ -4,8 +4,14 @@ import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class RegisterView implements IView {
     private Consumer<Type> onSceneChange;
@@ -26,8 +32,26 @@ public class RegisterView implements IView {
         new TerminalPasswordBox().attachTo(contentPanel);
         new TerminalText("Repeat password").attachTo(contentPanel);
         new TerminalPasswordBox().attachTo(contentPanel);
-        new TerminalButton("Register", () ->
-                MessageDialog.showMessageDialog(gui, "Success", "You may now login into your account.", MessageDialogButton.OK)
+        new TerminalButton("Register", () -> {
+            //MessageDialog.showMessageDialog(gui, "Success", "You may now login into your account.", MessageDialogButton.OK)
+            RequestBuilder builder = new RequestBuilder();
+            HttpJsonRequest req = builder.withGet().withJson("""
+                    {
+                      "username": "admin2",
+                      "password": "admin2"
+                    }
+                    """).withRoute("/register").build();
+            try (Socket client = new Socket("127.0.0.1", 6666);
+                 PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+                req.send(out);
+                MessageDialog.showMessageDialog(gui, "Response", in.lines().collect(Collectors.joining("\n")), MessageDialogButton.OK);
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         ).attachTo(contentPanel);
         new TerminalButton("Back to login page", () -> {
             window.close();
