daeebe244453302ca029747628f6352bb8248747
qexik <qexik@hotmail.com>
2023-07-24 16:58:43 +0500

    Fix indentation

--- src/main/java/oop/course/client/Client.java
+++ src/main/java/oop/course/client/Client.java
@@ -20,48 +20,48 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         new Client(
-                new TerminalGUI(
+            new TerminalGUI(
+                List.of(
+                    new LoginView(
+                        new TerminalWindow(
+                            "BankSystem login",
+                            new Panel(
+                                new LinearLayout(Direction.VERTICAL)
+                            )
+                        ),
                         List.of(
-                                new LoginView(
-                                        new TerminalWindow(
-                                                "BankSystem login",
-                                                new Panel(
-                                                        new LinearLayout(Direction.VERTICAL)
-                                                )
-                                        ),
-                                        List.of(
-                                                new TerminalText(
-                                                        "Welcome to the BankSystem client application!\nPlease, register or login into your existing account."
-                                                ),
-                                                new TerminalForm(
-                                                        List.of(
-                                                                new TerminalFormKeyValuePair(
-                                                                        "email",
-                                                                        new TerminalInputPair(
-                                                                                new TerminalText("Email"),
-                                                                                new TerminalTextBox()
-                                                                        )
-                                                                ),
-                                                                new TerminalFormKeyValuePair(
-                                                                        "password",
-                                                                        new TerminalInputPair(
-                                                                                new TerminalText("Password"),
-                                                                                new TerminalPasswordBox()
-                                                                        )
-                                                                )
-                                                        )
-                                                ),
-                                                new TerminalButton("Login", () -> {}),
-                                                new TerminalButton("Register page", () -> {}),
-                                                new TerminalButton("Exit", () -> {})
+                            new TerminalText(
+                                "Welcome to the BankSystem client application!\nPlease, register or login into your existing account."
+                            ),
+                            new TerminalForm(
+                                List.of(
+                                    new TerminalFormKeyValuePair(
+                                        "email",
+                                        new TerminalInputPair(
+                                            new TerminalText("Email"),
+                                            new TerminalTextBox()
                                         )
+                                    ),
+                                    new TerminalFormKeyValuePair(
+                                        "password",
+                                        new TerminalInputPair(
+                                            new TerminalText("Password"),
+                                            new TerminalPasswordBox()
+                                        )
+                                    )
                                 )
+                            ),
+                            new TerminalButton("Login", () -> {}),
+                            new TerminalButton("Register page", () -> {}),
+                            new TerminalButton("Exit", () -> {})
                         )
-                ),
-                new SocketServerBridge(
-                        "127.0.0.1",
-                        6666
+                    )
                 )
+            ),
+            new SocketServerBridge(
+                "127.0.0.1",
+                6666
+            )
         ).run();
     }
 
