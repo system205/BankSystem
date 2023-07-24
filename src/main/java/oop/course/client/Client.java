a8de90e949d1ec84dcc0cce6361afe36c4f33f96
qexik <qexik@hotmail.com>
2023-07-24 15:11:43 +0500

    Remove factories from code

--- src/main/java/oop/course/client/Client.java
+++ src/main/java/oop/course/client/Client.java
@@ -6,19 +6,15 @@ public class Client {
     private final GUI clientGui;
     private final ServerBridge serverBridge;
 
-    public Client(GUIFactory guiFactory, ServerBridgeFactory serverFactory) throws IOException {
-        try {
-            this.clientGui = guiFactory.bestGUIImplementation();
-            this.serverBridge = serverFactory.bestServerImplementation();
-        } catch (IOException e) {
-            throw new IOException("Unable to create GUI");
-        }
+    public Client(GUI clientGui, ServerBridge serverBridge) {
+        this.clientGui = clientGui;
+        this.serverBridge = serverBridge;
     }
 
     public static void main(String[] args) throws IOException {
         new Client(
-                new GUIFactory(),
-                new ServerBridgeFactory(
+                new TerminalGUI(),
+                new SocketServerBridge(
                         "127.0.0.1",
                         6666
                 )
--- src/main/java/oop/course/client/GUIFactory.java
+++
@@ -1,9 +0,0 @@
-package oop.course.client;
-
-import java.io.IOException;
-
-public class GUIFactory {
-    public GUI bestGUIImplementation() throws IOException {
-        return new TerminalGUI();
-    }
-}
